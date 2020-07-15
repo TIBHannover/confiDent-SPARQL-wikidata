6b5a8964bbc26343ef93c2e8dd05db6564d296ef
Castro0o <andre@andrecastro>
2020-07-15 17:30:05 +0200

    moved sparq file

--- dataimports/ingestlogic/wikidata.py
+++ dataimports/ingestlogic/wikidata.py
@@ -7,7 +7,8 @@ def sparql() -> Dict:
     sources_yaml = file_utils.yaml_get_source('../datasources/sources.yml')
     wikidata_yaml = sources_yaml['wikidata']
     endpoint = SPARQLWrapper(wikidata_yaml['sparqlendpoint'])
-    sparql_query = file_utils.relative_read_f('../sparql/wikidata.rq')
+    sparql_f = '../sparql/wikidata_series.rq'
+    sparql_query = file_utils.relative_read_f(sparql_f)
     endpoint.setQuery(sparql_query)
     endpoint.setReturnFormat(JSON)
     results = endpoint.query().convert()
