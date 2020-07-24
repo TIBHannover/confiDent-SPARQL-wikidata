c23af10f770f8939b1783133e98cb4d3bc08b8fc
Castro0o <andre@andrecastro>
2020-07-24 08:59:07 +0200

    handling results

--- dataimports/app.py
+++ dataimports/app.py
@@ -1,23 +1,25 @@
 from pprint import pprint
-
-from dataimports import sparql, file_utils
+from dataimports import sparql
 from dataimports.wikidata import wikidata
 
 
-def importdata(source: str,
-               outformat='stdout',
-               outfile=''):
+def importdata(source: str, outformat: str, outfile: str):
 
     # mapping = file_utils.yaml_get_mapping(mapping=source)
 
     if source == 'wikidata':
         results = sparql.query(source='wikidata',
                                query_='EventSeries')
+        print('**SPARL results: 1 item:**' )
+        pprint(results[0])
+        results = sparql.process_results(results=results,
+                                         source='wikidata',
+                                         out_format=outformat)
+        print('\n**Simplified Dict results: 1 item:**' )
+        pprint(results[0])
+
+
         # print(json.dumps(results))
-        for entry in results:
-            pprint(entry)
-            entry_simple_dict = wikidata.sparqlitem2dict(item=entry)
-            pprint(entry_simple_dict)
-            print('\n')
+
 
         print('Results returned:', len(results))
