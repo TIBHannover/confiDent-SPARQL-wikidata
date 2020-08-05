016233da5987660aad9b44866aee9652a0fe422c
Castro0o <andre@andrecastro>
2020-08-05 20:32:09 +0200

    many to one mapping prototype

--- prototypes/mapping.py
+++ prototypes/mapping.py
@@ -1,90 +1,117 @@
 from pprint import pprint
 
+# todo: in yaml/dict here: replace key property external_prop external_prop_external_prop_URI
+
 confident_map = {
     'Website': {
-        'property': 'official_website',
-        'URI': 'https://www.wikidata.org/wiki/Property:P856'
+        'external_prop': 'official_website',
+        'external_prop_URI': 'https://www.wikidata.org/wiki/Property:P856',
+        'range': ['Process', 'Agent']
     },
      'Twitter': {
-         'property': 'Twitter_username',
-         'URI': 'https://www.wikidata.org/wiki/Property:P2002'
-         }
+         'external_prop': 'Twitter_username',
+         'external_prop_URI': 'https://www.wikidata.org/wiki/Property:P2002',
+         'range': ['Process', 'Agent']
+         },
+    'Event Frequency': {
+        'external_prop': 'event_interval',
+        'external_prop_URI': 'https://www.wikidata.org/wiki/Property:P2257',
+        'range': ['Event Series']
+    }
 }
 
+
+def all_confid_ranges(mapdict):
+    allranges = []
+    for prop_dict  in mapdict.values():
+        range = prop_dict.get('range')
+        allranges += range
+    allranges = list(set(allranges))
+    return allranges
+
+
+allranges = all_confid_ranges(mapdict=confident_map)
+
+
 # challenge 1: invert dictionary
-
-
 def invert_dict(srcdict):
     destdict = {}
     for k, v in srcdict.items():
-        prop = v['property']
+        prop = v['external_prop']
         if prop not in destdict.keys():
             destdict[prop] = k
     return destdict
 
-inverted_confid_map = invert_dict(confident_map)
-print(inverted_confid_map)
 
-# challenge 2: structure for subobject:
+# challenge 2: structure confident_map to accommodated for subobject:
 # there is MORE THAN 1 PROPERTY per semantic suboject "cluster":
 #   ie Process Name, Process Name Type
-# there is a hierarchy between the propertyes
-#   * Process Name:
-#        * Process Name Type
-# the value of child property determines to which exte. property it
-#   will be mapped to
-# the properties will be same for the different instances of a subobject
 # the subobject is identified by its template name
 
 confident_map_subobjts = {
-    'Process Name-official name': {
-        'property': 'official name',
-        'child_prop_vals': [('Process Name Type', 'official name')]
+    'official name': {
+        'external_prop': 'itemLabel',
+        'child_prop_vals': [('Process Name Type', 'official name')],
+        'subobject': 'Subobject Process Name'
     },
-    'Process Name-acronym': {
-        'property': 'short name',
-        'child_prop_vals': [('Process Name Type', 'official name')]
+    'acronym': {
+        'external_prop': 'short_name',
+        'child_prop_vals': [('Process Name Type', 'acronym')],
+        'subobject': 'Subobject Process Name'
     },
 }
-
+# not the use of propety value tuples in child_prop_vals
 confident_map.update(confident_map_subobjts)
 pprint(confident_map)
-
-
-def invert_dict(srcdict):
-    destdict = {}
-    for k, v in srcdict.items():
-        keys = v.keys()
-        prop = v['property']
-        if prop not in destdict.keys():
-            destdict[prop] = k
-        else:
-            prop = v['property']
-            if prop not in destdict.keys():
-                destdict[prop] = k
-    return destdict
-
 inverted_confid_map = invert_dict(confident_map)
-print(inverted_confid_map)
+print('inverted_confid_map:', inverted_confid_map)
 
-# {'Twitter_username': 'Twitter',
-# 'official  name': 'Process Name-official
-# name', 'short name': 'Process Name-acronym'}
-# keys are correct
+wd_mock_data ={'official_website': 'https://nips.cc/',
+               'Twitter_username': 'NeurIPSConf',
+               'short_name': 'NeurIPS',
+               'Freebase_ID': '/m/07rwz3',
+               'event_interval': '1 year'
+               }
+print('data item (as captured from source - not mapped):', wd_mock_data)
 
-'''
-{{Subobject Process Name
-|Process Name=PIDapalooza 2020
-|Process Name Type=official name
-}}
 
-acronym=XYZ (1 prop wikidata
- \|/
-Process Name Type=acronym  (2 prop confIDent
-Process Name=XYZ
+# extract the values needed for confIDent properties
+def dataitem2confid(datadict, inv_confid_map):
+    datadict_confid = {}
+    # loop through prop:val(s) from ext. data source item (datadict)
+    for datadict_k, datadict_v in datadict.items():
+        if datadict_k in inv_confid_map.keys():
+            # from ext. data prop get corresponding confIDent property
+            confid_prop = inv_confid_map[datadict_k]
+            # store the external data source value, under the corresponding
+            # confIDent property
+            datadict_confid[confid_prop] = datadict_v
+    return datadict_confid
 
-{{Subobject Process Name
-|Process Name=PID(test)
-|Process Name Type=acronym
-}}
-'''
+
+dataitem_confid = dataitem2confid(datadict=wd_mock_data,
+                                  inv_confid_map=inverted_confid_map)
+print('data item mapped to confIDent:', dataitem_confid)
+
+# when producing the output with Jinja template
+# create first the none-subprocess templates
+for range_ in allranges:
+    print(f'-> {range_}')
+    for k, v in dataitem_confid.items():
+        confid_item_keys = confident_map[k].keys()
+        if 'subobject' not in confid_item_keys:
+            confid_item_range = confident_map[k]['range']
+            if range_ in confid_item_range:
+                print('range:', range_, ' / property:', k, '- val:', v)
+
+# and then create the the process templates
+for k, v in dataitem_confid.items():
+    if 'subobject' in confident_map[k].keys():
+        print('subobject in:', confident_map[k].keys())
+        subobj_template =  confident_map[k]['subobject']
+        subobj_child_propvals =  confident_map[k]['child_prop_vals']
+        print(f'-> {subobj_template}')
+        print(subobj_child_propvals)
+
+
+
