03eb287d32f3902fbc8efb16fccf75075a677ce0
Castro0o <andre@andrecastro>
2020-08-10 09:53:57 +0200

    dataimports/mapping checkout back to commit 88bd3bc529591d18a9c4be869237e405b64c8cf3

--- dataimports/mapping.py
+++ dataimports/mapping.py
@@ -17,31 +17,10 @@ def schema2confi_map(schema: str, schema_data: List) -> Dict:
     for schema_data_item in schema_data:
         for extschema_k in schema_data_item:
             if extschema_k not in mapping_schema2confident:
-                for confid_k, confid_map_i in confident_mapping.items():
-                    if type(confid_map_i) is dict and extschema_k in confid_map_i['property']:
-                        if 'child_confid_prop' not in confid_map_i.keys():
-                            # why extschema_k and not confid_map_i['property'] ?
-                            mapping_schema2confident[extschema_k] = confid_k
-                            break
-                        else:  # subobject
-                            # mapping_schema2confident[confid_map_i['property']] = confid_map_i['parent_confid_prop']
-
-                            import pdb; pdb.set_trace()
-
-
-                    # elif type(confid_map_i) is list:  # subojects list
-                    #     for suboject in confid_map_i:
-                    #         # subobject = {'official name':
-                    #         # {'property': 'itemLabel',
-                    #         # 'Process Name Type': 'official  name'}}
-                    #         extschema_k = suboject.values[0].get('property')
-                    #
-                    #         pprint(suboject)
-                    #         mapping_schema2confident[extschema_k] = confid_k
-                    #
-                    #         import pdb; pdb.set_trace()
-                                                
-                        
+                for confid_k, confid_dict in confident_mapping.items():
+                    if confid_dict and extschema_k in confid_dict['property']:
+                        mapping_schema2confident[extschema_k] = confid_k
+                        break
     return mapping_schema2confident
 
 
