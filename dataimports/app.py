bce06aec12f470381d714f942ef48710e79365ac
Castro0o <andre@andrecastro>
2020-08-24 09:54:06 +0200

    loop_sparql_results as def

--- dataimports/app.py
+++ dataimports/app.py
@@ -4,6 +4,32 @@ from dataimports import sparql
 from dataimports.file_utils import createglobals
 
 
+def loop_sparql_results(source: str, class_: str, outformat: str, outfile: str,
+                        limit: int, write: bool):
+    for i, result in enumerate(
+            iterable=sparql.query(source=source, class_=class_),
+            start=1):
+        if limit and i > limit:
+            break
+        # print('\n', '**SPARL result:**', type(result))
+        # pprint(result)
+        result_title, result_formatted = sparql.process_result(dataitem=result,
+            source=source, out_format=outformat,
+            class_=class_)  # TODO: class_ come from _source.yml
+
+        current_result_i = i
+        if outformat == 'wiki' and write:
+            mwactions.edit(page=result_title,
+                           content=result_formatted + '[[Category:Test]]',
+                           newpageonly=False,
+                           summary="Edited by confIDent Data Importer")
+        else:
+            print(result_formatted)
+
+    return f'Source:{source} class:{class_}, returned:{current_result_i} ' \
+           f'results'
+
+
 def importdata(source: str, outformat: str, outfile: str, limit: int,
                write: bool):
     outfile = outfile
@@ -11,27 +37,7 @@ def importdata(source: str, outformat: str, outfile: str, limit: int,
 
     if source == 'wikidata':
         createglobals(source='wikidata')
-        for i, result in enumerate(
-                iterable=sparql.query(source='wikidata',
-                                      class_='Event_Series'),
-                start=1):
-            if limit and i > limit:
-                break
-            # print('\n', '**SPARL result:**', type(result))
-            # pprint(result)
-            result_title, result_formatted = sparql.process_result(
-                dataitem=result,
-                source='wikidata',
-                out_format=outformat,
-                class_='Event_Series')  # TODO: class_ come from _source.yml
-
-            current_result_i = i
-            if outformat == 'wiki' and write:
-                mwactions.edit(page=result_title,
-                               content=result_formatted + '[[Category:Test]]',
-                               newpageonly=False,
-                               summary="Edited by confIDent Data Importer")
-            else:
-                print(result_formatted)
-
-        print(f'Results returned: {current_result_i}')
+        summary = loop_sparql_results(source='wikidata', class_='Event_Series',
+                                      outformat=outformat, outfile=outfile,
+                                      limit=limit, write=write)
+        print(summary)
